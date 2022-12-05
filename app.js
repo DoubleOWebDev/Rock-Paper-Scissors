7f6f815b8abb4dbbc3f1c0d08c57e2533d63b873
Osasuyi Osawe <oajo8732@gmail.com>
2022-12-05 15:53:29 -0800

    Created Choice funct and Rock paper scissors funct

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
 //12/05/22 03:16PM Start Time
-let choices = ['Rock' , 'Paper' , 'Scissors'];
+
 function getComputerChoice() {
-    
+    let choices = ['Rock' , 'Paper' , 'Scissors'];
     let choice = choices[Math.floor(Math.random()*choices.length)];
     return choice;
 }
 
-console.log(getComputerChoice())
+console.log(getComputerChoice());
+
+function playGame(playerSelection , computerSelection){ 
+     computerSelection = getComputerChoice();
+
+    if (playerSelection === 'Rock' && computerSelection === 'Rock' || playerSelection === 'Paper' && computerSelection === 'Paper' || playerSelection === 'Scissors' && computerSelection === 'Scissors'){
+        return 'IT\'s A DRAW... What are you doing?!';
+
+    }else if (playerSelection === 'Scissors' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Rock' || playerSelection === 'Rock' && computerSelection === 'Rock') {
+        return 'YOU WIN!!! YOU\'RE A WEINER!';
+
+    }else{
+        return 'YOU LOSE! BETTER LUCK NEXT TIME!!';
+    }
+}
+console.log(playGame('Rock'))
