ab5fb7579748ffecc7a4bc295c7783b3d7076d53
Osasuyi Osawe <oajo8732@gmail.com>
2022-12-06 22:46:29 -0800

    Creating game function

--- app.js
+++ app.js
@@ -7,9 +7,8 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
 
-function playGame(playerSelection , computerSelection){ 
+const playRound = (playerSelection , computerSelection)=>{ 
     computerSelection = getComputerChoice();
     playerSelection = playerSelection.toUpperCase();
 
@@ -22,4 +21,25 @@ function playGame(playerSelection , computerSelection){
     }else{
         return 'YOU LOSE! BETTER LUCK NEXT TIME!!';
     }
+}
+
+//12/06/22 10:02PM Start Time
+// 12/06/22 ---- End Time
+
+
+//Set playRound to loop 5 times
+let userScore = parseInt(0);
+let computerScore = parseInt(0);
+let win = "You win"
+let lose = "You lose"
+let tie = "It is a tie"
+
+const game = () =>{
+    for(var i=0;i<5;i++){
+        let playerSelection = prompt("Pick a move");
+        const computerSelection = getComputerChoice()
+        console.log(playRound(playerSelection, computerSelection))
+        console.log("your score = " + userScore);
+        console.log("Computer's score = " + computerScore);
+      }      
 }
