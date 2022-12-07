318c2bf65f281fcf23aee1f820ed730f0cfff047
Osasuyi Osawe <oajo8732@gmail.com>
2022-12-07 01:16:25 -0800

    Stuck on completion of loop in game function

--- app.js
+++ app.js
@@ -23,23 +23,36 @@ const playRound = (playerSelection , computerSelection)=>{
     }
 }
 
+
 //12/06/22 10:02PM Start Time
-// 12/06/22 ---- End Time
+// 12/07/22 1:16AM End Time
 
+/* I first declared the scores. Then created the game function. After, i created a do while loop to do my playRound function while the player and compScore is less than 5. The conditions within my do loop SHOULD be added increments of 1 to the player and comp score until it equals 5.*/
 
-//Set playRound to loop 5 times
-let userScore = parseInt(0);
-let computerScore = parseInt(0);
-let win = "You win"
-let lose = "You lose"
-let tie = "It is a tie"
+/****    My problem is occurring because I have an infinite loop going on as well as getting an error statement of 'Cannot read properties of null (reading'toUpperCase)' in which case I am also lost on why that is       ******/
+let playerScore = 0;
+let compScore = 0;
 
-const game = () =>{
-    for(var i=0;i<5;i++){
-        let playerSelection = prompt("Pick a move");
-        const computerSelection = getComputerChoice()
-        console.log(playRound(playerSelection, computerSelection))
-        console.log("your score = " + userScore);
-        console.log("Computer's score = " + computerScore);
-      }      
+const game = () => {
+do {
+playRound()
+
+if (playerScore === 'YOU WIN!!! YOU\'RE A WEINER!'){
+ playerScore += 1;
+}else if (compScore === 'YOU LOSE! BETTER LUCK NEXT TIME!!'){
+ compScore +=1;
+}else {
+ 'IT\'s A DRAW... What are you doing?!';
 }
+}
+while (playerScore <= 5 && compScore <= 5)
+console.log(playerScore)
+console.log(compScore)
+//Have prompt take a playRound parameter***ALREADY INSIDE ORIGINAL ROUND FUNCTION***
+//Loop through a single playRound
+
+//Keep score each 1 round
+
+//Declare the winner or loser after 5 loops
+}
+
