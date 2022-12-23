df032aabfbf9293e04f0512150cc149bdfb78dd8
Osasuyi Osawe <oajo8732@gmail.com>
2022-12-23 09:03:01 -0800

    Add click events to pictures

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ let playerScore = 0
 let computerScore = 0
 
 let  playRound = (playerSelection , computerSelection) =>{ 
-    let weaponChoice = prompt('Choose your Weapon!!!');
+
+    let weaponChoice = playerSelection;
     computerSelection = getComputerChoice();
     playerSelection = weaponChoice.trim().toUpperCase();
   
@@ -53,7 +54,34 @@ const game = (round) =>{
         Box of images to put game choices 
     
     Connecting JS to DOM:
-        {Add eventlistener and function to each picture}
+        Add eventlistener to each picture
+        Make click equal a round
+        Create redo game function
+        Create redo
           Display result in h2 when clicked
           Display score in h2 when clicked
 */
+//Add eventlistener to each picture
+const btnRock = document.querySelector('.btnRock').addEventListener('click', () =>{
+  alert('rock');
+});
+
+
+
+const btnPaper = document.querySelector('.btnPaper').addEventListener('click', () =>{
+  alert('paper');
+});
+
+
+
+const btnScissors = document.querySelector('.btnScissors').addEventListener('click', () =>{
+  alert('Scissors');
+});
+
+//Add eventlistener to each picture
+/*In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+For now, remove the logic that plays exactly five rounds.
+Create three buttons, one for each selection. Add an event listener to the buttons that call your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+Add a div for displaying results and change all of your console.logs into DOM methods.
+Display the running score, and announce a winner of the game once one player reaches 5 points.
+You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.*/
