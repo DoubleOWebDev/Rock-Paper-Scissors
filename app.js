e228f35cde120c4bd295700d3624c2dc5cf2a7fc
Osasuyi Osawe <oajo8732@gmail.com>
2022-12-23 09:47:41 -0800

    Add click events to images

--- app.js
+++ app.js
@@ -1,29 +1,66 @@
+/*ROCK PAPER SCISSORS PSUEDO CODE
+    Creating UI:
+       Create title header
+        Create h2 space for results
+        {Create space to display results of both   players choices}
+        Display score
+        Box of images to put game choices 
+    
+    Connecting JS to DOM:
+        Add eventlistener to each picture
+        Make click equal a round
+        Create redo game function
+        Create redo
+          Display result in h2 when clicked
+          Display score in h2 when clicked
+*/
+//Add eventlistener to each picture
+const btnRock = document.querySelector('.btnRock').addEventListener('click', () =>{
+  document.querySelector('.imageChoiceContainerPlayer').textContent = '🪨yesyesysye';
+});
+
+//display your choice in DOM
+
+
+const btnPaper = document.querySelector('.btnPaper').addEventListener('click', () =>{
+  document.querySelector('.imageChoiceContainerPlayer').textContent = '🧻';
+});
+
+
+
+const btnScissors = document.querySelector('.btnScissors').addEventListener('click', () =>{
+  document.querySelector('.imageChoiceContainerPlayer').textContent = '✂️';
+});
+
+
 function getComputerChoice() {
-    let choices = ['ROCK' , 'PAPER' , 'SCISSORS'];
-    let choice = choices[Math.floor(Math.random()*choices.length)];
-    return choice;
+  let choices = ['ROCK' , 'PAPER' , 'SCISSORS'];
+  let choice = choices[Math.floor(Math.random()*choices.length)];
+  return choice;
 }
+
 let playerScore = 0
 let computerScore = 0
 
 let  playRound = (playerSelection , computerSelection) =>{ 
 
-    let weaponChoice = playerSelection;
-    computerSelection = getComputerChoice();
-    playerSelection = weaponChoice.trim().toUpperCase();
-  
-    if (playerSelection === computerSelection){
-        return 'IT\'s A DRAW... What are you doing?!';        
+  let weaponChoice = playerSelection;
+  computerSelection = getComputerChoice();
+  playerSelection = weaponChoice.trim().toUpperCase();
 
-    }else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-        playerScore++;
-        return 'YOU WIN!!! YOU\'RE A WEINER!';
-        
-    }else{
-        computerScore++;
-        return 'YOU LOSE! BETTER LUCK NEXT TIME!!';
-    }
+  if (playerSelection === computerSelection){
+      return 'IT\'s A DRAW... What are you doing?!';        
+
+  }else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+      playerScore++;
+      return 'YOU WIN!!! YOU\'RE A WEINER!';
+      
+  }else{
+      computerScore++;
+      return 'YOU LOSE! BETTER LUCK NEXT TIME!!';
+  }
 }
+
 
 
 const game = (round) =>{
@@ -43,41 +80,6 @@ const game = (round) =>{
     return 'IT IS A DRAW'
   }
 }
-
-
-/*ROCK PAPER SCISSORS PSUEDO CODE
-    Creating UI:
-       Create title header
-        Create h2 space for results
-        {Create space to display results of both   players choices}
-        Display score
-        Box of images to put game choices 
-    
-    Connecting JS to DOM:
-        Add eventlistener to each picture
-        Make click equal a round
-        Create redo game function
-        Create redo
-          Display result in h2 when clicked
-          Display score in h2 when clicked
-*/
-//Add eventlistener to each picture
-const btnRock = document.querySelector('.btnRock').addEventListener('click', () =>{
-  alert('rock');
-});
-
-
-
-const btnPaper = document.querySelector('.btnPaper').addEventListener('click', () =>{
-  alert('paper');
-});
-
-
-
-const btnScissors = document.querySelector('.btnScissors').addEventListener('click', () =>{
-  alert('Scissors');
-});
-
 //Add eventlistener to each picture
 /*In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
 For now, remove the logic that plays exactly five rounds.
